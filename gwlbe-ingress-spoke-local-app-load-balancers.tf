cdf276a9fb59c46035babd9a0412763f0251bb79
Jun Ye <jye@aviatrix.com>
2022-03-28 17:34:21 -0400

    refactor alb

--- gwlbe-ingress-spoke-local-app-load-balancers.tf
+++ gwlbe-ingress-spoke-local-app-load-balancers.tf
@@ -22,101 +22,105 @@ resource "aws_lb_target_group" "gwlbe_ingress_spoke_local_app_nlb_tg" {
 
 
 resource "aws_lb_target_group_attachment" "gwlbe_ingress_spoke_local_app_nlb_tg_attachment" {
-  for_each = merge([for app in keys(aws_lb_target_group.gwlbe_ingress_spoke_local_app_nlb_tg) : {for zone in module.gwlbe_ingress_spoke_instance: "${app}-${zone.instance_id}" => {app=app,instance_id=zone.instance_id} }]...)
+  for_each         = merge([for app in keys(aws_lb_target_group.gwlbe_ingress_spoke_local_app_nlb_tg) : { for zone in module.gwlbe_ingress_spoke_instance : "${app}-${zone.instance_id}" => { app = app, instance_id = zone.instance_id } }]...)
   target_group_arn = aws_lb_target_group.gwlbe_ingress_spoke_local_app_nlb_tg[each.value["app"]].arn
   target_id        = each.value["instance_id"]
   port             = 80
 }
 
 resource "aws_lb_listener" "gwlbe_ingress_spoke_local_app_nlb_listener" {
-    for_each = aws_lb.gwlbe_ingress_spoke_local_app_nlb
-    load_balancer_arn = each.value.arn
-    port = "80"
-    protocol = "TCP"
-    default_action {
+  for_each          = aws_lb.gwlbe_ingress_spoke_local_app_nlb
+  load_balancer_arn = each.value.arn
+  port              = "80"
+  protocol          = "TCP"
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.gwlbe_ingress_spoke_local_app_nlb_tg[each.key].arn
   }
 }
 
-output "gwlbe_ingress_local_app_nlb_dns_name" {    
-    value = [for nlb in aws_lb.gwlbe_ingress_spoke_local_app_nlb : nlb.dns_name]
-    
+output "gwlbe_ingress_local_app_nlb_dns_name" {
+  value = [for nlb in aws_lb.gwlbe_ingress_spoke_local_app_nlb : nlb.dns_name]
+
 }
 
 
-# resource "aws_security_group" "gwlbe_ingress_spoke_local_app_alb_sg" {
-#   name        = "${var.vpc_name}-local-app-alb-sg"
-#   description = "${var.vpc_name}-local-app-alb-sg"
-#   vpc_id      = aws_vpc.ingress.id
+resource "aws_security_group" "gwlbe_ingress_spoke_local_app_alb_sg" {
+  for_each    = module.nlb_subnets
+  name        = "${var.vpc_name}-${each.key}-local-app-alb-sg"
+  description = "${var.vpc_name}-${each.key}-local-app-alb-sg"
+  vpc_id      = aws_vpc.ingress.id
 
-#    ingress {
-#     description = "TCP80"
-#     from_port   = 0
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  ingress {
+    description      = "TCP80"
+    from_port        = 0
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   tags = {
-#     Name = "${var.vpc_name}-local-app-alb-sg"
-#   }
-# }
-
-
-# resource "aws_lb" "gwlbe_ingress_spoke_local_app_alb" {
-#   name               = "${var.vpc_name}-local-app-alb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   subnets            = [for subnet in module.nlb_subnets.nlb_subnets : subnet.subnet_id]
-#   security_groups    = [aws_security_group.gwlbe_ingress_spoke_local_app_alb_sg.id]
-
-#   enable_deletion_protection = false
-
-#   tags = {
-#     Name = "${var.vpc_name}-local-app-alb"
-#   }
-# }
+  tags = {
+    Name = "${var.vpc_name}-${each.key}-local-app-alb-sg"
+  }
+}
 
 
+resource "aws_lb" "gwlbe_ingress_spoke_local_app_alb" {
+  for_each           = aws_security_group.gwlbe_ingress_spoke_local_app_alb_sg
+  name               = "${var.vpc_name}-${each.key}-local-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [for subnet in module.nlb_subnets[each.key].nlb_subnets : subnet.subnet_id]
+  security_groups    = [each.value.id]
 
+  enable_deletion_protection = false
 
-# resource "aws_lb_target_group" "gwlbe_ingress_spoke_local_app_alb_tg" {
-#   name     = "${var.vpc_name}-local-app-alb-tg"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = aws_vpc.ingress.id
-# }
-
-
-# resource "aws_lb_target_group_attachment" "gwlbe_ingress_spoke_local_app_alb_tg_attachment" {
-#   count = length(module.gwlbe_ingress_spoke_instance)
-#   target_group_arn = aws_lb_target_group.gwlbe_ingress_spoke_local_app_alb_tg.arn
-#   target_id        = module.gwlbe_ingress_spoke_instance[count.index].instance_id
-#   port             = 80
-# }
-
-# resource "aws_lb_listener" "gwlbe_ingress_spoke_local_app_alb_listener" {
-#     load_balancer_arn = aws_lb.gwlbe_ingress_spoke_local_app_alb.arn
-#     port = "80"
-#     protocol = "HTTP"
-#     default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.gwlbe_ingress_spoke_local_app_alb_tg.arn
-#   }
-# }
+  tags = {
+    Name = "${var.vpc_name}-${each.key}-local-app-alb"
+  }
+}
 
 
 
-# output "gwlbe_ingress_local_app_alb_dns_name" {
-#     value = aws_lb.gwlbe_ingress_spoke_local_app_alb.dns_name
-# }
+
+resource "aws_lb_target_group" "gwlbe_ingress_spoke_local_app_alb_tg" {
+  for_each = module.nlb_subnets
+  name     = "${var.vpc_name}-${each.key}-local-app-alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.ingress.id
+}
+
+
+resource "aws_lb_target_group_attachment" "gwlbe_ingress_spoke_local_app_alb_tg_attachment" {
+  for_each         = merge([for app in keys(aws_lb_target_group.gwlbe_ingress_spoke_local_app_alb_tg) : { for zone in module.gwlbe_ingress_spoke_instance : "${app}-${zone.instance_id}" => { app = app, instance_id = zone.instance_id } }]...)
+  target_group_arn = aws_lb_target_group.gwlbe_ingress_spoke_local_app_alb_tg[each.value["app"]].arn
+  target_id        = each.value["instance_id"]
+  port             = 80
+}
+
+resource "aws_lb_listener" "gwlbe_ingress_spoke_local_app_alb_listener" {
+    for_each          = aws_lb.gwlbe_ingress_spoke_local_app_alb
+    load_balancer_arn = each.value.arn
+    port = "80"
+    protocol = "HTTP"
+    default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.gwlbe_ingress_spoke_local_app_alb_tg[each.key].arn
+  }
+}
+
+
+
+output "gwlbe_ingress_local_app_alb_dns_name" {
+    value = [for nlb in aws_lb.gwlbe_ingress_spoke_local_app_alb : nlb.dns_name]
+}
